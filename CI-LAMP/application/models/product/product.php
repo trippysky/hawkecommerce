3e7cba34a601e55fac1dd95312fee480a5ee5168
Tom LaMotte <lamotte@gmail.com>
2015-10-26 17:24:20 -0700

    added product model, wrote nasty sql query

---
+++ CI-LAMP/application/models/product/product.php
@@ -0,0 +1,17 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+	
+	class Product extends CI_Model
+	{
+
+		public function get_orders()
+		{
+			//orders.id, 
+			//customers.first_name, customers.last_name, 
+			//order.created_at, orders.status
+			//billaddresses.street_1, billaddresses.street_2, billaddresses.city, billaddresses.state, billaddresses.zip
+			//order_items.qty,
+			//products.price
+		}
+
+	}
+?>
